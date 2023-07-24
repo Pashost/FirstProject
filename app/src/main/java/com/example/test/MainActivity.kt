d76fb943af781ef0cc2183ff95156f3154d58239
Pasha <nikolaevpasa4@gmail.com>
2023-07-24 03:43:24 +0300

    Transition from one activity to another

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.second_activity)
+        setContentView(R.layout.activity_main)
+
+        val loginButton: Button = findViewById(R.id.button3)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, SeccondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/test/SeccondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.test
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+class SeccondActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+
+    }
+}
